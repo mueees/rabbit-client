c719a5ce92e960908670b893487b1579ef5d5670
mue <mue.miv@gmail.com>
2015-10-25 18:07:11 +0200

    add base routes

--- app/scripts/pages/main/main.route.js
+++ app/scripts/pages/main/main.route.js
@@ -7,9 +7,27 @@
                 url: '/main',
                 isLoginRequired: true,
                 templateUrl: 'app/scripts/pages/main/main.view.html',
-                controller: 'MainController',
-                resolve: {
-                }
+                controller: 'MainController'
+            })
+
+            // when user select certain feed
+            .state('app.feed', {
+                url: '/feed'
+            })
+
+            // page with all topics
+            .state('app.discovery', {
+                url: '/discovery'
+            })
+
+            // create new category, move feed to another category
+            .state('app.organize', {
+                url: '/organize'
+            })
+
+            // search result, show posts from feed in right sidebar
+            .state('app.search', {
+                url: '/search'
             });
     });
 })();
