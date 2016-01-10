0ce13d9b2835374ae718b71b8d00463826a52bb1
mue <mue.miv@gmail.com>
2016-01-10 12:11:30 +0200

    enable production environment

--- src/app/scripts/rb.config.service.js
+++ src/app/scripts/rb.config.service.js
@@ -3,7 +3,7 @@
 
     angular.module('rb').provider('rbConfig', function () {
         var config = {
-            /*environment: 'production'*/
+            environment: 'production'
         };
 
         var developmentOrigin = 'http://localhost:10002',
